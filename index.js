075354d7f881fe6141d5cf6694becf090c260e76
Loris <loris4526@gmail.com>
2017-05-27 16:02:04 +0200

    Style toggleable menu

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 $(".navbar-toggleable-md").on("show.bs.collapse", function () {
   console.log("Element is opened");
-  $("#top-logo").css("margin-bottom","15px");
-  const topMenuStyles = {
-    "background-color": "#252a2c",
-    "height": "100vh",
-  };
-  $(".fixed-top-menu").css(topMenuStyles);
-  // $(".white-link").css("color","#dcfd00");
+  $("#top-logo").css("margin-bottom","10px");
+  $(".fixed-top-menu").addClass("collapsing-menu");
+  $(".fa-user-o,.fa-search").removeClass("hidden-md-down");
 })
 
 $(".navbar-toggleable-md").on("hidden.bs.collapse", function () {
   console.log("Element is closed");
   $("#top-logo").css("margin-bottom","-47px");
+  $(".fixed-top-menu").removeClass("collapsing-menu");
+  $(".fa-user-o,.fa-search").addClass("hidden-md-down");
 })
