07b9294472118e76d634d927a2dba922ef2f391c
Loris <loris4526@gmail.com>
2017-06-06 14:42:13 +0200

    Initialize jQuery with document.ready()

--- javascript/index.js
+++ javascript/index.js
@@ -1,27 +1,29 @@
-$(".navbar-toggleable-md").on("show.bs.collapse", function () {
-  console.log("Element is opened");
-  $("#top-logo").css("margin-bottom","10px");
-  $(".fixed-top-menu").addClass("collapsing-menu");
-  $(".fa-user-o,.fa-search").removeClass("hidden-md-down");
-})
+$(document).ready(function(){
+  $(".navbar-toggleable-md").on("show.bs.collapse", function () {
+    console.log("Element is opened");
+    $("#top-logo").css("margin-bottom","10px");
+    $(".fixed-top-menu").addClass("collapsing-menu");
+    $(".fa-user-o,.fa-search").removeClass("hidden-md-down");
+  })
 
-$(".navbar-toggleable-md").on("hidden.bs.collapse", function () {
-  console.log("Element is closed");
-  $("#top-logo").css("margin-bottom","-47px");
-  $(".fixed-top-menu").removeClass("collapsing-menu");
-  $(".fa-user-o,.fa-search").addClass("hidden-md-down");
-})
+  $(".navbar-toggleable-md").on("hidden.bs.collapse", function () {
+    console.log("Element is closed");
+    $("#top-logo").css("margin-bottom","-47px");
+    $(".fixed-top-menu").removeClass("collapsing-menu");
+    $(".fa-user-o,.fa-search").addClass("hidden-md-down");
+  })
 
 
-//Setting Scrolling height of the carousel
-function changeScrollingHeight(){
-  let totalHeight = $(window).height();
-  let belowCarousel = $(".main-page-hidden-content").height();
-  let stringBelowCarousel = `${belowCarousel + totalHeight + 50}px`;
+  //Setting Scrolling height of the carousel
+  function changeScrollingHeight(){
+    let totalHeight = $(window).height();
+    let belowCarousel = $(".main-page-hidden-content").height();
+    let stringBelowCarousel = `${belowCarousel + totalHeight + 50}px`;
 
-  document.body.style.setProperty("--below-carousel",stringBelowCarousel);
-};
+    document.body.style.setProperty("--below-carousel",stringBelowCarousel);
+  };
 
-//Call the changeScrollingHeight function both initially and whenever the device's viewPort changes
-changeScrollingHeight();
-$(window).resize(changeScrollingHeight);
+  //Call the changeScrollingHeight function both initially and whenever the device's viewPort changes
+  changeScrollingHeight();
+  $(window).resize(changeScrollingHeight);
+});
